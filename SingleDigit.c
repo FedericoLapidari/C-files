ed9a0e947abd060629dd2393fda3240a050053f0
Federico Lapidari <federicolapidari@gmail.com>
2025-09-08 20:59:59 +0200

    Refactor SingleDigit function for efficiency
    Refactor SingleDigit function to use a do-while loop for digit summation until a single-digit result is obtained.

--- SingleDigit.c
+++ SingleDigit.c
@@ -1,23 +1,21 @@
+/*Write a subroutine that, given an integer as input, returns a single-digit integer corresponding to the sum 
+of the digits that make up the input value, repeated also on intermediate values until the sum results in a
+single-digit number. Assume that the input value can be arbitrarily large (without causing overflow) and is 
+guaranteed to be positive.*/
+
 #include <stdio.h>
 #define BASE 10
 
 int SingleDigit(int n){
-    int count, tmp;
-    count=0;
-    while(n!=0){
-        count+=(n%BASE);
-        n/=BASE;
-    }
-    tmp=0;
-    while(count>=BASE){
-        tmp+=count%BASE;
-        count/=BASE;
-    }
-
-    if(tmp!=0){
-        count=tmp;
-    }
-
-    return count;
+    int ris;
+    do {
+        ris = 0;
+        while (n > 0) {
+            ris += n % BASE;
+            n /= BASE;
+        }
+        n = ris;
+    } while (ris >= BASE);
+    return ris;
     
 }
